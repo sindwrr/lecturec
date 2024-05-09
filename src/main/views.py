1f7ba7c123e1c19ce72c4247d67da2469878949f
kitsunin-dev <stepanpilipcenko@gmail.com>
2024-05-09 14:53:16 +0300

    Added transcribing, styles for index page

--- src/main/views.py
+++ src/main/views.py
@@ -1,16 +1,27 @@
-from django.http import HttpRequest
+import os
+
+import whisper
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from pytube import YouTube
 
 
 def process_file(f):
-    with open(f"uploads/{f.name}", "wb+") as dest:
+    upload_dir = "uploads/"
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+
+    file_path = os.path.join(upload_dir, f.name)
+    with open(file_path, "wb+") as dest:
         for chunk in f.chunks():
             dest.write(chunk)
+    return file_path
 
 
-# Create your views here.
 def index(request: HttpRequest):
     if request.method == "POST":
-        process_file(request.FILES["file_upload"])
+        filename = process_file(request.FILES["file_upload"])
+        model = whisper.load_model("base")
+        result = model.transcribe(filename)
+        return HttpResponse(result["text"])
+
     return render(request, "index.html")
